1fa1f79a87eb8489f5643244f486a1add7e46a14
Chris Myers <cmyers78@gmail.com>
2016-09-20 14:31:46 -0400

    programmatically creating mapView & Constraints.

--- Ch3/WorldTrotterCM/WorldTrotterCM/MapViewController.swift
+++ Ch3/WorldTrotterCM/WorldTrotterCM/MapViewController.swift
@@ -11,6 +11,46 @@ import MapKit
 
 class MapViewController : UIViewController {
     
+    var mapView : MKMapView!
+    
+    override func loadView() {
+        mapView = MKMapView()
+        
+        view = mapView
+        
+        // creates a segmented control to choose map types
+        let segmentControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
+        // segment control's background color
+        segmentControl.backgroundColor = UIColor.whiteColor().colorWithAlphaComponent(0.5)
+        // defaults to Standard map type
+        segmentControl.selectedSegmentIndex = 0
+        
+        // set to false so as not to mess up constraints
+        segmentControl.translatesAutoresizingMaskIntoConstraints = false
+        
+        segmentControl.addTarget(self, action: #selector(self.mapTypeChanged(_:)), forControlEvents: .ValueChanged)
+        
+        // adding segment control to the view screen
+        view.addSubview(segmentControl)
+        
+        // MARK: Creating and setting anchors
+        
+        // creates constraints
+        let topConstraint = segmentControl.topAnchor.constraintEqualToAnchor(topLayoutGuide.bottomAnchor, constant: 8)
+//        let leadingConstraint = segmentControl.leadingAnchor.constraintEqualToAnchor(view.leadingAnchor)
+//        let trailingConstraint = segmentControl.trailingAnchor.constraintEqualToAnchor(view.trailingAnchor)
+        
+        // creates margins instead of constraints
+        let margins = view.layoutMarginsGuide
+        let leadingConstraint = segmentControl.leadingAnchor.constraintEqualToAnchor(margins.leadingAnchor)
+        let trailingConstraint = segmentControl.trailingAnchor.constraintEqualToAnchor(margins.trailingAnchor)
+        // activates constraints
+        
+        topConstraint.active = true
+        leadingConstraint.active = true
+        trailingConstraint.active = true
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -19,12 +59,18 @@ class MapViewController : UIViewController {
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
-        
-                
-        
-        
-        
-        
     }
     
+    func mapTypeChanged(segControl : UISegmentedControl) {
+        switch segControl.selectedSegmentIndex {
+        case 0:
+            mapView.mapType = .Standard
+        case 1:
+            mapView.mapType = .Hybrid
+        case 2:
+            mapView.mapType = .Satellite
+        default:
+            break
+        }
+    }
 }
